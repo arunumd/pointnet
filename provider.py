e9bd8bc159777c78e0b714b86afb5bdba1f891b9
Arun Kumar D <32076623+arunumd@users.noreply.github.com>
2019-10-03 15:34:18 -0400

    Changed modelnet files location

--- provider.py
+++ provider.py
@@ -10,9 +10,9 @@ DATA_DIR = os.path.join(BASE_DIR, 'data')
 if not os.path.exists(DATA_DIR):
     os.mkdir(DATA_DIR)
 if not os.path.exists(os.path.join(DATA_DIR, 'modelnet40_ply_hdf5_2048')):
-    www = 'https://shapenet.cs.stanford.edu/media/modelnet40_ply_hdf5_2048.zip'
+    www = '/content/drive/My Drive/modelnet40_ply_hdf5_2048.zip'
     zipfile = os.path.basename(www)
-    os.system('wget %s; unzip %s' % (www, zipfile))
+    os.system('mv %s; unzip %s' % (www, zipfile))
     os.system('mv %s %s' % (zipfile[:-4], DATA_DIR))
     os.system('rm %s' % (zipfile))
 
